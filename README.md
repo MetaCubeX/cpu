12599f28311b63796c7efdc77270454163615bbd
wwqgtxx <wwqgtxx@gmail.com>
2026-02-25 21:55:05 +0800

    sync go1.26.0

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 backport cpu for go1.20+
 
 export `internal/cpu` from:
-https://github.com/golang/go/tree/e4216d54f58616b862013e2810436462db08b6ed/src/internal/cpu
+https://github.com/golang/go/tree/go1.26.0/src/internal/cpu
--- cpu.go
+++ cpu.go
@@ -21,29 +21,43 @@ var CacheLineSize uintptr = CacheLinePadSize
 // in addition to the cpuid feature bit being set.
 // The struct is padded to avoid false sharing.
 var X86 struct {
-	_            CacheLinePad
-	HasAES       bool
-	HasADX       bool
-	HasAVX       bool
-	HasAVX2      bool
-	HasAVX512F   bool
-	HasAVX512BW  bool
-	HasAVX512VL  bool
-	HasBMI1      bool
-	HasBMI2      bool
-	HasERMS      bool
-	HasFSRM      bool
-	HasFMA       bool
-	HasOSXSAVE   bool
-	HasPCLMULQDQ bool
-	HasPOPCNT    bool
-	HasRDTSCP    bool
-	HasSHA       bool
-	HasSSE3      bool
-	HasSSSE3     bool
-	HasSSE41     bool
-	HasSSE42     bool
-	_            CacheLinePad
+	_                   CacheLinePad
+	HasAES              bool
+	HasADX              bool
+	HasAVX              bool
+	HasAVXVNNI          bool
+	HasAVX2             bool
+	HasAVX512           bool // Virtual feature: F+CD+BW+DQ+VL
+	HasAVX512F          bool
+	HasAVX512CD         bool
+	HasAVX512BW         bool
+	HasAVX512DQ         bool
+	HasAVX512VL         bool
+	HasAVX512GFNI       bool
+	HasAVX512VAES       bool
+	HasAVX512VNNI       bool
+	HasAVX512VBMI       bool
+	HasAVX512VBMI2      bool
+	HasAVX512BITALG     bool
+	HasAVX512VPOPCNTDQ  bool
+	HasAVX512VPCLMULQDQ bool
+	HasBMI1             bool
+	HasBMI2             bool
+	HasERMS             bool
+	HasFSRM             bool
+	HasFMA              bool
+	HasGFNI             bool
+	HasOSXSAVE          bool
+	HasPCLMULQDQ        bool
+	HasPOPCNT           bool
+	HasRDTSCP           bool
+	HasSHA              bool
+	HasSSE3             bool
+	HasSSSE3            bool
+	HasSSE41            bool
+	HasSSE42            bool
+	HasVAES             bool
+	_                   CacheLinePad
 }
 
 // The booleans in ARM contain the correspondingly named cpu feature bit.
@@ -155,8 +169,15 @@ var RISCV64 struct {
 //go:linkname S390X
 //go:linkname RISCV64
 
+// doDerived, if non-nil, is called after processing GODEBUG to set "derived"
+// feature flags.
+var doDerived func()
+
 func init() {
 	doinit()
+	if doDerived != nil {
+		doDerived()
+	}
 }
 
 // options contains the cpu debug options that can be used in GODEBUG.
